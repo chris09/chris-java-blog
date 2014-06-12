56fef9f50642e18588d8442ad8f93dcaf8697709
Chris Yu <chris09.yu@gmail.com>
2014-06-13 06:20:57 +0800

    Spring web app tutorial 29: Twitter Bootstrap modal

--- src/main/java/tw/com/chris/controller/UsreController.java
+++ src/main/java/tw/com/chris/controller/UsreController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import tw.com.chris.entity.Blog;
 import tw.com.chris.entity.User;
+import tw.com.chris.service.BlogServicec;
 import tw.com.chris.service.UserService;
 
 @Controller
@@ -19,9 +21,17 @@ public class UsreController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BlogServicec blogService;
+	
 	@ModelAttribute("user")
-	public User construct() {
+	public User constructUser() {
 		return new User();
+	}
+	
+	@ModelAttribute("blog")
+	public Blog constructBlog() {
+		return new Blog();
 	}
 	
 	@RequestMapping("/users")
@@ -52,6 +62,13 @@ public class UsreController {
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
 		return "user-detail";
+	}
+	
+	@RequestMapping(value="/account", method=RequestMethod.POST)
+	public String doRegistrer(@ModelAttribute("blog") Blog blog, Principal principal) {
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
 	}
 	
 }
---
+++ src/main/java/tw/com/chris/service/BlogServicec.java
@@ -0,0 +1,28 @@
+package tw.com.chris.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tw.com.chris.entity.Blog;
+import tw.com.chris.entity.User;
+import tw.com.chris.repository.BlogRepository;
+import tw.com.chris.repository.UserRepository;
+
+@Service
+public class BlogServicec {
+	
+	@Autowired
+	private BlogRepository blogRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public void save(Blog blog, String name) {
+		User user = userRepository.findByName(name);
+		blog.setUser(user);
+		blogRepository.save(blog);
+	}
+
+	
+	
+}
