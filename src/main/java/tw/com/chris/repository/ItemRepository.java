5019972659850a952ef16cf97213d86d5ac02576
Chris Yu <chris09.yu@gmail.com>
2014-06-13 01:51:19 +0800

    Spring web app tutorial 17: Spring Data JPA Paging and sorting

--- src/main/java/tw/com/chris/repository/ItemRepository.java
+++ src/main/java/tw/com/chris/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package tw.com.chris.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tw.com.chris.entity.Blog;
@@ -9,7 +10,6 @@ import tw.com.chris.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 	
-	List<Item> findByBlog(Blog blog);
-	
+	List<Item> findByBlog(Blog blog, Pageable pageable);
 	
 }
